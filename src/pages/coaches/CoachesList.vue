75b31939cf391cf70e874bf65fdc923cdc1ea339
Stanislaw Zaremba <stanislaw.zaremba94@gmail.com>
2021-02-21 12:46:44 +0100

    first commit

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    FILTER
+  </section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul>
+      LIST of COACHESs
+    </ul>
+  </section>
+</template>
