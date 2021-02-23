5fd1a401dc5479806d4078c3c9dbd06da8c434d3
Stanislaw Zaremba <stanislaw.zaremba94@gmail.com>
2021-02-23 20:23:38 +0100

    Addin Fetch Data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button v-if="!isCoach" link to="/register"
           >Register as Coach</base-button
         >
@@ -37,6 +37,7 @@ export default {
   },
   data() {
     return {
+      isLoading: false,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -67,9 +68,16 @@ export default {
       return this.$store.getters['coaches/hasCoaches'];
     }
   },
+  created() {
+    this.loadCoaches();
+  },
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
+    },
+    loadCoaches() {
+      this.isLoading = true;
+      this.$store.dispatch('coaches/loadCoaches');
     }
   }
 };
