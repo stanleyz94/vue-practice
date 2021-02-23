385880c7b4c4ba0f8a88686176ba26ba80cef40a
Stanislaw Zaremba <stanislaw.zaremba94@gmail.com>
2021-02-23 18:44:00 +0100

    Adding Request Store

--- src/pages/requests/RequestReceived.vue
+++ src/pages/requests/RequestReceived.vue
@@ -1,3 +1,15 @@
 <template>
-  REQUESTS
+  <section>
+    <base-card>
+      <header>
+        <h2>Request Received</h2>
+      </header>
+      <ul>
+        <h3>You haven't received any requests yet!</h3>
+        <li></li>
+      </ul>
+    </base-card>
+  </section>
 </template>
+
+<style scoped></style>
