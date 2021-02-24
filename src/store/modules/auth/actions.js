4409c2c99f957a9f999bc681b1750aeb0cb29a0b
Stanislaw Zaremba <stanislaw.zaremba94@gmail.com>
2021-02-24 19:51:56 +0100

    Adding login & signup prototype

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,24 +1,58 @@
 export default {
-  login() {},
-  async signup(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvOcmh_Avvu08bFdUHdmJzA06c6vV4h0E', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true
-      })
-    });
+  async login(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBLS0NnffDTjxGYFji8VMyAERabwB6J_Ag',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true
+        })
+      }
+    );
 
     const responseData = await response.json();
 
     if (!response.ok) {
       console.log(responseData);
-      const error = new Error(responseData.message || 'Failed to authenticate.');
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.'
+      );
       throw error;
     }
-
     console.log(responseData);
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn
+    });
+  },
+  async signup(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBLS0NnffDTjxGYFji8VMyAERabwB6J_Ag',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.'
+      );
+      throw error;
+    }
+    console.log(responseData);
+
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
